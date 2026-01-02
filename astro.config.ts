a8615f7ecb944abb708f0b152a1fd4bdda42e9dd
madoreenu <madoreenu314@gmail.com>
2026-01-03 02:03:28 +0900

    akirame

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import websiteConfig from 'astro-spaceship/config';
 
 export default defineConfig({
   site: 'https://madoreenu314.github.io',
-  base: '/til',
+  base: '/til/',
   // ここにテーマの設定を流し込む必要があります
   integrations: [
     astroSpaceship(websiteConfig)
