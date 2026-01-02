84d5b69f1ece6ca13c44ce1694dfe6734eb008eb
madoreenu <madoreenu314@gmail.com>
2026-01-02 18:40:40 +0900

    bug fix

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
-
 import { defineConfig } from 'astro/config';
 import { astroSpaceship } from 'astro-spaceship';
-
+// テーマの設定を読み込む
 import websiteConfig from 'astro-spaceship/config';
 
 export default defineConfig({
   site: 'https://madoreenu314.github.io',
-  base: '/til', 
+  base: '/til',
+  // ここにテーマの設定を流し込む必要があります
+  integrations: [
+    astroSpaceship(websiteConfig)
+  ],
 });
-
